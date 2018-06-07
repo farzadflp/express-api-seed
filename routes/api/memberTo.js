3bfdac3ca81295390a0ae6cc4e44bc1a3f96d708
Farzad Falahpour <farzad@farzad.local>
2018-06-07 17:15:45 +0430

    student can member to a course

---
+++ routes/api/memberTo.js
@@ -0,0 +1,25 @@
+let express = require('express');
+let router = express.Router();
+let User = require('../../models/user.model');
+let Course = require('../../models/course.model');
+let { auth, permit } = require('../../functions/authentication');
+
+router.post('/:id', auth, permit('student'), (req, res) => {
+    Course.update({ _id: req.params.id }, { $push: { members: req.body.id } }).then((course) => {
+        res.send({
+            status: 'success',
+            data: {
+                message: "you added to course",
+                yourId: req.body.id
+            }
+        });
+    }).catch(error => {
+        res.send({
+            status:'error',
+            error: err 
+        });
+});
+
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ router.use('/api/users' , require('./api/users') );
 router.use('/api/courses', require('./api/course') );
 router.use('/api/courses/events' , require('./api/event') );
 router.use('/api/users/sendTo' , require('./api/sendTo') );
-//router.use('/api/users/memberTo' , require('./api/memberTo') );
+router.use('/api/users/memberTo' , require('./api/memberTo') );
 
 router.all('/', (req, res) => {
   res.send({
